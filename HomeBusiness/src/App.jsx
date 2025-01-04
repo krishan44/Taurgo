34b9a18ab6c2675280743cf4497bba4ade2f470a
krishan44 <124803438+krishan44@users.noreply.github.com>
2025-01-04 22:09:48 +0530

    Initialize React application with Vite setup and basic components

---
+++ HomeBusiness/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import Login from "./Components/login/login";  
+import CreateAccount from "./Components/clientRegistration/createAccount";  
+
+function App() {
+  return (
+    <>
+        <CreateAccount/>
+    </>
+  )
+}
+
+export default App
---
+++ HomeBusiness/src/Components/login/login.jsx
@@ -0,0 +1,42 @@
+import style from './login.module.css';
+import logoTaurgo from "../../assets/loginPage/logoTaurgo.png";
+import loginPhoto from "../../assets/loginPage/loginPhoto.jpeg";
+import RICS from "../../assets/loginPage/RICS.png";
+
+function Login() {
+    return(
+        <div className={style.mainContainer}>
+            <div className={style.inputContainer}>
+                <img src={logoTaurgo} alt="Logo" className={style.logo} /> 
+                <div className={style.loginFields}>
+                    <h2>Login</h2>
+                    <span className={style.welcomeText}>Welcome Back!</span>
+                    <span className={style.fieldName}>Email</span>
+                    <input type="text" placeholder='Enter your Email' required />
+                    <span className={style.fieldName}>Password</span>
+                    <input type="password" placeholder='********' required/>
+                    <div className={style.Options} >
+                        <div className="remember"> 
+                            <input type="checkbox" id="rememberMe" name="rememberMe"/>
+                            <label htmlFor="rememberMe" className={style.remeberText}>Remember for 30 Days</label>
+                        </div>
+                        <a href="https://github.com/krishan44?tab=overview&from=2025-01-01&to=2025-01-04" className={style.forgotPassword}>Forgot Password</a>
+                    </div>
+                    <a href=""><button className={style.signinBtn}>Sign In</button></a>
+                    <span className={style.registerText}>Don't have an Account? <a href="">Register</a></span>
+
+                </div>
+            </div>
+            <div className={style.bottomElements}>
+                <span className={style.copyright}>© Taurgo</span>
+                <img src={RICS} alt="Tech Partner Logo" className={style.techPartner}/>
+            </div>
+            <div className={style.imageContainer}>
+                <img src={loginPhoto} alt="Houses" className={style.loginPhoto} />
+                
+            </div>
+        </div>
+    )
+}
+
+export default Login;
