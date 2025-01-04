d22520de530db4556bad305a4dbbe108be0aef8b
krishan44 <124803438+krishan44@users.noreply.github.com>
2025-01-05 01:09:10 +0530

    Add Organisation Details section to PersonalDetail component with styling

--- HomeBusiness/src/Components/clientRegistration/PersonalDetail.jsx
+++ HomeBusiness/src/Components/clientRegistration/PersonalDetail.jsx
@@ -11,8 +11,61 @@ function PersonalDetail() {
                           <div> <span className={style.number}>4 </span><span>Fit To Work <span className={style.dash}>|</span></span> </div>
                           <div> <span className={style.number}>5 </span><span>Onbording Form Complete</span> </div>
                       </div>
-
-        </div>
+                <div className={style.Details}>
+                    <span className={style.title}>Organisation Details</span>
+                    <div className={style.inputGrid}>
+                        <div className={style.inputFields}>
+                            <span className={style.fieldName}>
+                                Company Name<span className={style.required}>*</span>
+                            </span>
+                            <br />
+                            <input 
+                                type="text" 
+                                className={style.datafield}  
+                                placeholder='First Name' 
+                                required 
+                            />
+                        </div>
+                        <div className={style.inputFields}>
+                            <span className={style.fieldName}>
+                                Full Name<span className={style.required}>*</span>
+                            </span>
+                            <br />
+                            <input 
+                                type="tel" 
+                                className={style.datafield}  
+                                placeholder='Full Name' 
+                                required 
+                            />
+                        </div>
+                        <div className={style.inputFields}>
+                            <span className={style.fieldName}>
+                                Email<span className={style.required}>*</span>
+                            </span>
+                            <br />
+                            <input 
+                                type="text" 
+                                className={style.datafield}  
+                                placeholder='hello@email.com' 
+                                required 
+                            />
+                        </div>
+                        <div className={style.inputFields}>
+                            <span className={style.fieldName}>
+                                Phone<span className={style.required}>*</span>
+                            </span>
+                            <br />
+                            <input 
+                                type="text" 
+                                className={style.datafield}  
+                                placeholder='Add your phone number' 
+                                required 
+                            />
+                        </div>
+                    </div>
+                    <a href="" className={style.btnNext}>Next</a>
+                </div>
+            </div>
         </>
     )
 }
